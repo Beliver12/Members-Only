6da4a0402c05c992e0470355378300cdee42410d
Nikola <nzencic@gmail.com>
2024-10-29 14:03:04 +0100

    trying to make deploy on railway work

--- app.js
+++ app.js
@@ -82,5 +82,5 @@ app.use("/", membersRouter);
   ); 
 
   
-
-app.listen(3000, () => console.log("app listening on port 3000!"));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
